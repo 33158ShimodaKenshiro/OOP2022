d38a49f85d5bdb50737ac32449caf6268a1c81c5
infosys <infosys@C4218>
2022-04-22 14:10:13 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter02/DistanceConverter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter {
+    class Program {
+        static void Main(string[] args) {
+            if (args.Length >= 1 && args[0] == "-tom") {
+                PrintFeetToMeterList(1, 10);
+            }
+            else {
+
+                PrintFeetToMeterList(1, 10);
+                }
+
+            }
+        private static void PrintFeetToMeterList(int start, int stop) {
+            for (int feet = start;feet <= 10; feet++) {
+                double meter = FeetToMeter(feet);
+                Console.WriteLine("{0} ft = {1:0.0000}ft", feet, meter);
+            }
+
+            }
+            private static void PrintMeterToFeetList(int start, int stop) {
+                for (int meter = 1; meter <= 10; meter++) {
+                    double feet = MeterToFeet(meter);
+                    Console.WriteLine("{0} ft = {1:0.0000}ft", feet, meter);
+                }
+
+
+                //メートルからフィートを求める
+                static double MeterToFeet(int meter) {
+                return meter / 0.3048;
+            }
+                }
+                static double MeterToFeet(int meter) {
+                    return meter / 0.3048;
+                }
+            }
+        }
+
