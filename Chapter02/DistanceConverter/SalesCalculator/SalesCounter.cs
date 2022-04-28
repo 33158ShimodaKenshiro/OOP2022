09f6b562f5b68211d2d5d7417548b0cf217e08a6
33158 下田健士郎 <33158@ojs.ac.jp>
2022-04-28 15:51:53 +0900

    インターフェイス

--- Chapter02/DistanceConverter/SalesCalculator/SalesCounter.cs
+++ Chapter02/DistanceConverter/SalesCalculator/SalesCounter.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace SalesCalculator {
     public class SalesCounter {
-        private List<Sale> _sales; //csvファイルから読み込んだデータ
+        private IEnumerable<Sale> _sales; //csvファイルから読み込んだデータ
 
 
         //コンストラクタ
@@ -16,7 +16,7 @@ namespace SalesCalculator {
         }
 
         //店舗別売上を求める
-        public Dictionary<string, int> GetPerStoreSales() {
+        public IDictionary<string, int> GetPerStoreSales() {
             Dictionary<string, int> dict = new Dictionary<string, int>();
             foreach (Sale sale in _sales) {
                 if (dict.ContainsKey(sale.ShopName))
@@ -28,7 +28,7 @@ namespace SalesCalculator {
 
         }
         //売上データを読み込み,saleオブジェクトのリストを返す
-        public static List<Sale> ReadSales(string filePath) {
+        public static IEnumerable<Sale> ReadSales(string filePath) {
             List<Sale> sales = new List<Sale>();
             string[] lines = File.ReadAllLines(filePath);
             foreach (string line in lines) {
