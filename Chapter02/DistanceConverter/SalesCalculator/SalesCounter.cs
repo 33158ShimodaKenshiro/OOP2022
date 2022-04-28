cae329d26aabfbff41b24c323d74e5dc1ababf4b
33158 下田健士郎 <33158@ojs.ac.jp>
2022-04-28 15:17:23 +0900

    p71 プログラムを改良

--- Chapter02/DistanceConverter/SalesCalculator/SalesCounter.cs
+++ Chapter02/DistanceConverter/SalesCalculator/SalesCounter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,21 +11,42 @@ namespace SalesCalculator {
 
 
         //コンストラクタ
-        public SalesCounter(List<Sale> sales) {
-            _sales = sales;
+        public SalesCounter(string filePath) {
+            _sales = ReadSales(filePath);
         }
 
         //店舗別売上を求める
-        public Dictionary<string,int> GetPerStoreSales() {
+        public Dictionary<string, int> GetPerStoreSales() {
             Dictionary<string, int> dict = new Dictionary<string, int>();
             foreach (Sale sale in _sales) {
                 if (dict.ContainsKey(sale.ShopName))
-                        dict[sale.ShopName] += sale.Amount;
+                    dict[sale.ShopName] += sale.Amount;
                 else
                     dict[sale.ShopName] = sale.Amount;
             }
             return dict;
 
+        }
+        //売上データを読み込み,saleオブジェクトのリストを返す
+        public static List<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+
+            }
+
+
+
+
+
+            return sales;
         }
     }
 }
