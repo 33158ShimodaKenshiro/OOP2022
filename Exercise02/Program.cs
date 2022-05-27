c510fcf3dd374bff90d45ed93be2a6eab44da734
33158 下田健士郎 <33158@ojs.ac.jp>
2022-05-27 13:39:37 +0900

    chapter05 Exercise02

--- Exercise02/Program.cs
+++ Exercise02/Program.cs
@@ -7,6 +7,13 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
+            var line = Console.ReadLine();
+            int num; //整数値を入れる変数を用意
+            if (int.TryParse(line, out num)) {
+                Console.WriteLine("{0:#,#}",num);//整数への変換成功
+            }　else {
+                Console.WriteLine("数値文字列ではありません");//変換失敗
+
 
         }
     }
