14d05975873221c8d335103c33be2a07e179345e
33158 下田健士郎 <33158@ojs.ac.jp>
2022-10-06 15:58:00 +0900

    1-5

--- Chapter15/Exercise1/Program.cs
+++ Chapter15/Exercise1/Program.cs
@@ -25,24 +25,62 @@ namespace Exercise1 {
         }
 
         private static void Exercise1_2() {
+            var max = Library.Books.Max(b => b.Price);
+            var book = Library.Books.First(b => b.Price == max);
+            Console.WriteLine(book);
+
+
+
         }
 
         private static void Exercise1_3() {
+            var query = Library.Books.GroupBy(b => b.PublishedYear);
+
+
+
+            foreach (var item in query.OrderBy(b => b.Key)) {
+
+                Console.WriteLine("{0}年{1}冊", item.Key, item.Count());
+            }
         }
+
 
         private static void Exercise1_4() {
-        }
+            var query = Library.Books
+                .Join(Library.Categories,
+                book => book.CategoryId,
+                category = > category.Id,
+                (book, category) = new {
+                    book,
+                    Title,
+                }
+                }
+                
 
         private static void Exercise1_5() {
-        }
 
-        private static void Exercise1_6() {
-        }
+            var query = Library.Books
+                    .Where(b => b.PublishedYear == 2016)
+                    .Join(
+                        Library.Categories, book => book.CategoryId,
+                        category => category => category.Name)
+                    .Distinct();
+            foreach (var name in query)
+                Console.WriteLine(name);
+            }
 
-        private static void Exercise1_7() {
-        }
 
-        private static void Exercise1_8() {
+            private static void Exercise1_6() {
+
+            }
+
+            private static void Exercise1_7() {
+
+            }
+
+            private static void Exercise1_8() {
+
+            }
         }
     }
 }
