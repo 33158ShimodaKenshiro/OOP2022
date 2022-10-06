783c4a5186fb11c9f5972392524f97b746444384
33158 下田健士郎 <33158@ojs.ac.jp>
2022-10-06 11:31:26 +0900

    Chapter15

---
+++ Chapter13/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Chapter13 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Chapter13/SampleEntityFramework1/Models/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleEntityFramework1.Models {
+    internal sealed class Configuration : DbMigrationsConfiguration<BooksDbContext> {
+        public Configuration() {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+            ContextKey = " SampleEntityFramework1.Models.BookDbContext";
+        }
+    }
+}
