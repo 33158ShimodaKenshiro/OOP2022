9d74900ff9210f96393005c112c26ff9a4acf887
33158 下田健士郎 <33158@ojs.ac.jp>
2022-10-20 11:18:41 +0900

    SampleSlider完成

--- WPF/SampleApplication/MainWindow.xaml.cs
+++ WPF/SampleApplication/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace SampleApplication {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
@@ -20,10 +21,73 @@ namespace SampleApplication {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            seasonComboBox.SelectedIndex = getSeasonIndex();
+            
+        }
+        private int getSeasonIndex() {
+            var today = DateTime.Now;
+            
+            int ret = 0;
+            switch (today.Month) {
+
+                case 3:
+                case 4:
+                case 5:
+                    ret = 0;
+                    break;
+
+                case 6:
+                case 7:
+                case 8:
+                    ret = 1;
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    ret = 2;
+                    break;
+                case 12:
+                case 1:
+                case 2:
+                    ret = 3;
+                    break;
+
+            }
+
+            return ret;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e) {
             MessageBox.Show("ボタンが押された");
+        }
+
+        private void checkBox_Unchecked(object sender, RoutedEventArgs e) {
+            checkBoxTextBlock.Text = "未チェック";
+        }
+
+        private void redRadioButton_Checked(object sender, RoutedEventArgs e) {
+            colorTextBox.Text = "赤";
+
+        }
+
+        private void seasonComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            seasonTextBlock.Text = (string)((ComboBoxItem)seasonComboBox.SelectedItem).Content;
+        }
+
+        private void yellowRadioButton_Checked(object sender, RoutedEventArgs e) {
+            colorTextBox.Text = "黄";
+        }
+
+        private void blueRadioButton_Checked(object sender, RoutedEventArgs e) {
+            colorTextBox.Text = "青";
+        }
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e) {
+            checkBoxTextBlock.Text = "チェック済み";
+        }
+
+        private void checkBox_Unchecked_1(object sender, RoutedEventArgs e) {
+
         }
     }
 }
