387ed06f39bbd39e970813a19a07ec8bda20ca8c
33158 下田健士郎 <33158@ojs.ac.jp>
2022-10-24 14:35:46 +0900

    XML

--- CarReportSystem/CarReportSystem/Form1.cs
+++ CarReportSystem/CarReportSystem/Form1.cs
@@ -17,7 +17,7 @@ namespace CarReportSystem {
     public partial class Form1 : Form {
 
         //設定情報保存用オブジェクト
-        Settings settings = new Settings();
+        Settings settings = Settings.getInstance();
 
         //住所データ管理用リスト
         BindingList<CarReport> listCarReport = new BindingList<CarReport>();
