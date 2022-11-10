6a84bbe48e07e1195ef277e519febdf677dacfc0
33158 下田健士郎 <33158@ojs.ac.jp>
2022-11-10 11:49:59 +0900

    Color削除ボタン

--- WPF/ColarChecker/MainWindow.xaml.cs
+++ WPF/ColarChecker/MainWindow.xaml.cs
@@ -14,17 +14,19 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CollarChecker {
+
+
+namespace CollarCheck {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window {
-        public  MainWindow() {
 
+        List<MyColor> colorList = new List<MyColor>();
+
+        public MainWindow() {
             InitializeComponent();
-
             DataContext = GetColorList();
-
         }
 
         /// <summary>
@@ -36,6 +38,7 @@ namespace CollarChecker {
                 .Select(i => new MyColor() { Color = (Color)i.GetValue(null), Name = i.Name }).ToArray();
         }
 
+
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
             setColor();
         }
@@ -44,49 +47,72 @@ namespace CollarChecker {
             setColor(); //起動時に初期状態の設定値（R:0 G:0 B:0）から色を設定
         }
 
+
         //テキストボックスの値を元に色を設定
         private void setColor() {
             var r = byte.Parse(rValue.Text);
             var g = byte.Parse(gValue.Text);
             var b = byte.Parse(bValue.Text);
-            colorArea.Background = new SolidColorBrush(Color.FromRgb(r, g, b));
-            
+            Color color = Color.FromRgb(r, g, b);
+            colorArea.Background = new SolidColorBrush(color);
         }
 
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
-            var mycolor = (MyColor)((ComboBox)sender).SelectedItem;
-            //var color = mycolor.Color;
-            //var name = mycolor.Name;
 
-            rSlider.Value = mycolor.Color.R;
-            gSlider.Value = mycolor.Color.G;
-            bSlider.Value = mycolor.Color.B;
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+
+
+            rSlider.Value = ((MyColor)((ComboBox)sender).SelectedItem).Color.R;
+            gSlider.Value = ((MyColor)((ComboBox)sender).SelectedItem).Color.G;
+            bSlider.Value = ((MyColor)((ComboBox)sender).SelectedItem).Color.B;
             setColor();
         }
 
-        private void stockButton_Click(object sender, RoutedEventArgs e) {
-            stockList.Items.Add("R:" + rValue.Text + " G :" + gValue.Text + "B: " + bValue.Text);
 
+
+        private void stockButton_Click(object sender, RoutedEventArgs e) {
             MyColor stColor = new MyColor();
             var r = byte.Parse(rValue.Text);
             var g = byte.Parse(gValue.Text);
             var b = byte.Parse(bValue.Text);
-
             stColor.Color = Color.FromRgb(r, g, b);
 
+
+
+            //テキストボックスのRGB値から色名称があるかチェック
             var colorName = ((IEnumerable<MyColor>)DataContext)
-                                .Where(c =>c.Color.R == stColor.Color.R &&
-                                       c.Color.R == stColor.Color.R &&
-                                       c.Color.R == stColor.Color.B).FirstOrDefault();
-
-            stockList.Items.Add(colorName?.Name ?? "R:" + rValue.Text + "G:" + gValue.Text + "B: " + bValue.Text);
-            colorList.Insert(0, stColor);
-
+                                  .Where(c => c.Color.R == stColor.Color.R &&
+                                              c.Color.G == stColor.Color.G &&
+                                            c.Color.B == stColor.Color.B).FirstOrDefault();
+            stockList.Items.Insert(0, colorName?.Name ?? "R:" + r + " G:" + g + "B:" + b);
+             colorList.Insert(0, stColor);
         }
 
-        private void stockList_SelectionChanged(object sender, SelectionChangedEventArgs e) {
-            rSlider.Value = colorList[stockList.SelectedIndex].Color.R;
 
+
+        private void stockList_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+
+
+            if (stockList.SelectedIndex == -1) return;
+            rSlider.Value = colorList[stockList.SelectedIndex].Color.R;
+            gSlider.Value = colorList[stockList.SelectedIndex].Color.G;
+            bSlider.Value = colorList[stockList.SelectedIndex].Color.B;
+             setColor();
+        }
+
+
+
+        //Deleteボタンクリック
+        private void deleteButton_Click(object sender, RoutedEventArgs e) {
+
+
+
+              var delIndex = stockList.SelectedIndex;
+              if (delIndex == -1) return;
+           stockList.Items.RemoveAt(delIndex);
+            colorList.RemoveAt(delIndex);
         }
     }
 
