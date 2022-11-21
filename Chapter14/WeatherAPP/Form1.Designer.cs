b24857ab580e7081fd7bf7d7cbc9307810f62b17
33158 下田健士郎 <33158@ojs.ac.jp>
2022-11-21 15:56:33 +0900

    途中経過

--- Chapter14/WeatherAPP/Form1.Designer.cs
+++ Chapter14/WeatherAPP/Form1.Designer.cs
@@ -29,26 +29,32 @@ namespace WeatherAPP {
             this.cbTihou = new System.Windows.Forms.ComboBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // tbWeatherinfo
             // 
-            this.tbWeatherinfo.Location = new System.Drawing.Point(12, 300);
+            this.tbWeatherinfo.Location = new System.Drawing.Point(542, 298);
             this.tbWeatherinfo.Multiline = true;
             this.tbWeatherinfo.Name = "tbWeatherinfo";
-            this.tbWeatherinfo.Size = new System.Drawing.Size(715, 138);
+            this.tbWeatherinfo.Size = new System.Drawing.Size(233, 140);
             this.tbWeatherinfo.TabIndex = 0;
+            this.tbWeatherinfo.TextChanged += new System.EventHandler(this.tbWeatherinfo_TextChanged);
             // 
             // btWeatherGet
             // 
+            this.btWeatherGet.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btWeatherGet.BackgroundImage = global::WeatherAPP.Properties.Resources.istockphoto_531327234_612x612;
             this.btWeatherGet.Location = new System.Drawing.Point(306, 41);
             this.btWeatherGet.Name = "btWeatherGet";
             this.btWeatherGet.Size = new System.Drawing.Size(85, 30);
             this.btWeatherGet.TabIndex = 1;
             this.btWeatherGet.Text = "取得";
-            this.btWeatherGet.UseVisualStyleBackColor = true;
+            this.btWeatherGet.UseVisualStyleBackColor = false;
             this.btWeatherGet.Click += new System.EventHandler(this.btWeatherGet_Click);
             // 
             // cbTihou
@@ -76,15 +82,44 @@ namespace WeatherAPP {
             this.pictureBox2.TabIndex = 5;
             this.pictureBox2.TabStop = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(540, 283);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "天気概況";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(508, 203);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 19);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(249, 298);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(233, 140);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.TextChanged += new System.EventHandler(this.tbWeatherinfo_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.cbTihou);
             this.Controls.Add(this.btWeatherGet);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.tbWeatherinfo);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -103,6 +138,9 @@ namespace WeatherAPP {
         private System.Windows.Forms.ComboBox cbTihou;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
